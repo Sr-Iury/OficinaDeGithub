ba485b4e9bbc9debd56d26e9936da89be9e5489f
Sr-Iury <iury.silvafim@gmail.com>
2024-04-19 09:55:46 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program {
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Oficina de Github");
+
+        Console.Write("Insira um Numero: ");
+		double num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Insira um outro Numero: ");
+        double num2 = Convert.ToInt32(Console.ReadLine());
+
+
+        double soma = num1 + num2;
+
+        Console.WriteLine($"A soma dos números é {soma}");
+
+        Console.ReadKey();
+
+    }
+}
+
